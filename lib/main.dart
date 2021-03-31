d09dac247ecc914f74983432a40ee93ed5eeca72
Darrshana R <67387068+Darrshana@users.noreply.github.com>
2021-03-31 23:39:36 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'price_screen.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.red[400],
+          scaffoldBackgroundColor: Colors.grey[850]),
+      home: PriceScreen(),
+
+    );
+  }
+}
